2030b96e220aca0588710df4ed429c89186c41df
Fred <delon.frederic.pro.job@gmail.com>
2016-11-07 17:00:52 +0100

    end of job

--- js/app.js
+++ js/app.js
@@ -1,59 +1,48 @@
 $(document).ready(function(){
 	"use strict";
 
-
-
 	var me = {
-
 		html:null,
 		id:null,
-		json:'http://localhost:7000/crm.json',
-		client:null,
-
+		json:'http://localhost:7000/public/crm.json',
 
 		init: function(){
-//Débute le script !
-me.jsonAfficher();
-},
+			//Débute le script !
+			me.jsonAfficher();
+			$('form').on('submit', this.formulaireRécupe.bind(this));
+		},
+		jsonAfficher: function(){
+			// Affiche les infos du fichier "crm.json" dans la page index.html ( boucle ?)
+			$.get(me.json, function(data){
+				me.html = data.customers
+				for(var i = 0; i<me.html.length ; i++ ){
+					$('#ficheClient').append('<div><span>ID :'+me.html[i].id+'</span><div><span>Prénom :<span>'+me.html[i].first_name+'</span></div><div><span>Nom : '+me.html[i].last_name+'</span></div><div><span>company : '+me.html[i].company+'</span></div><div><span>Role : '+me.html[i].role+'</span></div><div><span>phone : '+me.html[i].phone+'</span></div><div><span>email : '+me.html[i].email+'</span></div><div><span>Description : '+me.html[i].description+'</span></div></div><br>'); 
 
-bouttons: function(){
-//Boutton pour afficher les informations clients du fichier json sur index.html 
-//Boutton pour changer de page html et afficher les formulaire 
-//Boutton pour récupéré les infos du formulaire et les ajouter dans le fichier " crm.json"
-},
-jsonRécupe: function(){
-// Réccupéré les informations du fichier "crm.Json" avec une boucle for qui parcour l'objet et son tableau
-},
-jsonAfficher: function(){
-	
-// Affiche les infos du fichier "crm.json" dans la page index.html ( boucle ?)
-$.get(me.json, function(data){
-	me.html = data.customers
-	for(var i = 0; i<me.html.length ; i++ ){
-		$('#ficheClient').append('<div><span>ID'+me.html[i].id+'</span><div><span>Prénom :'+" "+me.html[i].first_name+'</span></div><div><span>Nom'+me.html[i].last_name+'</span></div><div><span>company'+me.html[i].company+'</span></div><div><span>Role'+me.html[i].role+'</span></div><div><span>phone'+me.html[i].phone+'</span></div><div><span>email'+me.html[i].mail+'</span></div><div><span>Description'+me.html[i].description+'</span></div></div><br>'); 
-			// $('#list').append('<li>'+me.html[i].first_name+'</li>');
-
-		}
-	});
-},
-
-formulaireRécupe: function(){
-//stocké les information du formulaire dans une variable "client"
-},
-formulaireToJson: function(){
-// incrémenté dans le fichier " crm.json " les informations stocké du formulaire grace a la variable " client"
-},
-
-selectionéClients: function(){
-// afficher les données du fichier " crm.json" dans un select  et les afficher sur la page index.html
-},
-
-
-
-
-
-
-
-}
-me.init();
-	});   // end of code
+				}
+			});
+		},
+		formulaireRécupe: function(event){
+			//stocké les information du formulaire
+			event.preventDefault();
+			var prenom = $("#prenom").val();
+			var nom = $("#nom").val();
+			var phone = $("#phone").val();
+			var mail = $("#mail").val();
+			var content = $("#content").val();
+			this.formulaireToJson({first_name:prenom, last_name:nom, phone:phone, email:mail, description:content});
+		},
+		formulaireToJson: function(data){
+			// ajouté dans le fichier " crm.json " 
+			$.ajax({
+				type:"POST",
+				url:$("form").attr("action"),
+				data: data,
+				success : this.success
+			})
+		},
+		success: function(){
+			alert("save client !");
+		},
+	}
+	me.init();
+		});   // end of code
--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 var express = require('express');
-var app = express();
+var fs = require('fs');
 var bodyparser = require('body-parser');
+var app = express();
 
 app.use(express.static(__dirname + '/'));
 app.use(bodyparser());
 
 
-app.get('/', function(req,res) {
-var crmPath = __dirname + '/crm.json';
+app.post('/public', function(req, res){
+	var post = req.body;
+	var crmPath = __dirname + '/public/crm.json';
 
+	fs.readFile(crmPath, 'utf8', function(err, data){
+		
+		var content = JSON.parse(data); 
 
-	res.send("Salut coquine ! ")
+		content.customers.push({first_name:post.first_name, last_name:post.last_name,phone:post.phone,email:post.email,description:post.description});
+		var jsonified = JSON.stringify(content);
 
+		fs.writeFile(crmPath, jsonified, function(err){
+			if(err) {
+				alert("TRY AGAIN");
+			}
+		});
+	});
+	res.json({message:"Votre client a bien été ajouté", status:'ok'});
+	res.send("GG WP");
 });
 
 
