fa95cc365d05f6cae4d5931b3b552d9978210da2
Fred <delon.frederic.pro.job@gmail.com>
2016-11-08 16:11:17 +0100

    edit mod

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var express = require('express');
-var fs = require('fs');
-var bodyparser = require('body-parser');
-var app = express();
+// lié les outils demander -----------------------|
+var express = require('express'); // serveur      |
+var fs = require('fs');           // file system  |
+var bodyparser = require('body-parser'); //parse  |
+// fin -------------------------------------------|
+
+// variable d'exécution du server ---
+var app = express();              //|
+// fin ------------------------------ 
 
 app.use(express.static(__dirname + '/'));
 app.use(bodyparser());
@@ -14,24 +19,33 @@ app.post('/public', function(req, res){
 	fs.readFile(crmPath, 'utf8', function(err, data){
 		
 		var content = JSON.parse(data);
-		var idLen = content.customers[content.customers.length-1].id;
-		var nbID = idLen+1;
+
+		// incrementation de "id" dans le fichier crm.json -------------
+		var idLen = content.customers[content.customers.length-1].id;//|
+		var nbID = idLen+1;                                          //|
+		// Fin de l'incrementation -------------------------------------
+
+		// posté les données dans le fichier crm.json------------------------------------------------------------------------------------------------------------------------------------------------
+		content.customers.push({id:nbID,first_name:post.first_name, last_name:post.last_name,company:post.company,role:post.role,phone:post.phone,email:post.email,description:post.description});//|
+		// fin du post-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		
+		// transformé en sting la chaîne de caractere----
+		var jsonified = JSON.stringify(content);	  //|
+		// fin de la transformation --------------------- 
 
-		content.customers.push({id:nbID,first_name:post.first_name, last_name:post.last_name,company:post.company,role:post.role,phone:post.phone,email:post.email,description:post.description});
-		var jsonified = JSON.stringify(content);
-
-		fs.writeFile(crmPath, jsonified, function(err){
-			if(err) {
-				alert("TRY AGAIN");
-			}
-		});
+		// écrire dans le fichier crm.json ---------------
+		fs.writeFile(crmPath, jsonified, function(err){//|
+			if(err) {								   //|
+				alert("TRY AGAIN");                    //|
+			}										   //|
+		}); 										   //|	
+		// fin de l'écriture ----------------------------|
 	});
 	res.json({message:"Votre client a bien été ajouté", status:'ok'});
 	res.send("GG WP");
 });
 
 
-app.listen(7000, function(){
-	console.log(' go fast sur le port 7000 ! ')
+app.listen(7000, function(){  // port utilisé pour le server
+	console.log(' go fast sur le port 7000 ! ')  // verifier si il es bien fonctionel dans le terminal
 });
